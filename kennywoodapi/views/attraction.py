d6ae7e32938cb70867811f8cdd29ea9e32c0cc57
Christian Pippin <ianpippin@gmail.com>
2020-02-19 10:30:29 -0600

    crud for KennywoodAPI

--- kennywoodapi/views/attraction.py
+++ kennywoodapi/views/attraction.py
@@ -24,6 +24,17 @@ class AttractionSerializer(serializers.HyperlinkedModelSerializer):
 
 class Attractions(ViewSet):
 
+    def create(self, request):
+        new_attraction = Attraction()
+        new_attraction.name = request.data["name"]
+        new_attraction.area_id = request.data["area_id"]
+
+        new_attraction.save()
+
+        serializer = AttractionSerializer(new_attraction, context={'request': request})
+
+        return Response(serializer.data)
+
     def retrieve(self, request, pk=None):
         """Handle GET requests for single attraction
 
@@ -54,3 +65,38 @@ class Attractions(ViewSet):
         serializer = AttractionSerializer(attractions, many=True, context={'request': request})
 
         return Response(serializer.data)
+
+    
+    # handles PUT
+    def update(self, request, pk=None):
+      """Handle PUT requests for an attraction
+
+      Returns:
+          Response -- Empty body with 204 status code
+      """
+      attraction = Attraction.objects.get(pk=pk)
+      attraction.name = request.data["name"]
+      attraction.area_id = request.data["area_id"]
+      attraction.save()
+
+      return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    # handles DELETE
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single Attraction item
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            item = Attraction.objects.get(pk=pk)
+            item.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Attraction.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
